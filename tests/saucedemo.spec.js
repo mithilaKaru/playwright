a79664a25e903acf4a52043c352206ea971abe48
Mithila <mithilakarunarathne@gmail.com>
2024-12-03 06:50:24 +0530

    The Item selecting and adding to Cart is added to the scriipt

--- tests/saucedemo.spec.js
+++ tests/saucedemo.spec.js
@@ -6,4 +6,11 @@ test('Login test on SauceDemo', async ({ page }) => {
   await page.fill('#password', 'secret_sauce');
   await page.click('#login-button');
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  await page.click('#item_4_title_link');
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory-item.html?id=4');
+  await page.click('#add-to-cart');
+  await page.click('#shopping_cart_container');
+  await expect(page).toHaveURL('https://www.saucedemo.com/cart.html');
+
+
 });
